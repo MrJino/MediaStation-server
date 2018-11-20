c094ec1ea2068cfbb6849fd76353dc3796d46fc8
MrJino <for1self@gmail.com>
2018-11-20 15:41:32 +0900

    configure setting for tomcat

--- src/main/kotlin/noh/jinil/boot/MediaStationServerApplication.kt
+++ src/main/kotlin/noh/jinil/boot/MediaStationServerApplication.kt
@@ -1,10 +1,16 @@
 package noh.jinil.boot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class MediaStationServerApplication
+class MediaStationServerApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(MediaStationServerApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<MediaStationServerApplication>(*args)
